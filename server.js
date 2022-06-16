fc3eed2122deafdefdbb2caceef9287db3c57a58
laure-sterne <laure.sterne@gmail.com>
2022-06-16 15:50:40 +0200

    add csv file and read it

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const stringify = require('csv-stringify').stringify
+const parse = require('csv-parse').parse
+const os = require('os')
+const multer  = require('multer')
+const upload = multer({ dest: os.tmpdir() })
 const app = express()
 const port = 3000
 
@@ -6,8 +13,22 @@ app.get('/', function (req, res) {
     res.send('<html><body><h1>Hello World</h1></body></html>');
 });
 
-app.post('/createPeriodicTask', function (req, res) {
+app.post('/post-data', function (req, res) {
     res.send('POST Request');
+});
+
+app.post('/read', upload.single('file'), (req, res) => {
+    const file = req.file
+  
+    const data = fs.readFileSync(file.path)
+    parse(data, (err, records) => {
+      if (err) {
+        console.error(err)
+        return res.status(400).json({success: false, message: 'An error occurred'})
+      }
+  
+      return res.json({data: records})
+    })
 });
 
 app.put('/update-data', function (req, res) {
@@ -21,3 +42,6 @@ app.delete('/delete-data', function (req, res) {
 app.listen(port, function () {
     console.log(`Node server is running on port ${port}`);
 });
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
